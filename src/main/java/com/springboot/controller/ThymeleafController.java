6c354a706c9aae309c1c643aee23634e5045e109
zhaohuanzhen <zhaohuanzhen@126.com>
2017-11-03 17:04:48 +0800

    Signed-off-by: zhaohuanzhen <zhaohuanzhen@126.com>

--- src/main/java/com/springboot/controller/ThymeleafController.java
+++ src/main/java/com/springboot/controller/ThymeleafController.java
@@ -3,6 +3,8 @@ package com.springboot.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +14,12 @@ import com.springboot.entity.Person;
 @RequestMapping("/hello")
 public class ThymeleafController
 {
+    private static final Logger logger = LoggerFactory.getLogger(ThymeleafController.class);
+    
     @RequestMapping("/helloHtml")
     public String helloHtml(Map<String, Object> map)
     {
-
+        logger.info("ThymeleafController helloHtml");
         map.put("hello", "from TemplateController.helloHtml");
         return "helloThymeleaf";//spring-boot:run work ;java -jar work
     }
@@ -28,6 +32,7 @@ public class ThymeleafController
     @RequestMapping("/thymeleaf")
     public String index(Model model)
     {
+        logger.error("ThymeleafController thymeleaf");
         Person single = new Person("jack", 11);
         List<Person> people = new ArrayList<Person>();
         Person p1 = new Person("xx", 12);
