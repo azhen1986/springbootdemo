785fddeb22e24477b7ac5996ac40556c61efd207
azhen1986 <zhaohuanzhen@126.com>
2017-11-23 14:34:37 +0800

    add spring security pom

---
+++ src/main/java/com/springboot/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.springboot.security;
+
+/**  
+ * 类名称：SecurityConfig    
+ * 类描述：    
+ * 创建人：Admin    
+ * 创建时间：Nov 23, 2017 10:27:53 AM    
+ * 修改人：Admin
+ * 修改时间：Nov 23, 2017 10:27:53 AM    
+ * 修改内容：    
+ * @version 1.0   
+ */
+public class SecurityConfig
+{
+
+}
